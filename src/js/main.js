613ead321c8f8cae786ae38c47db67466bc855c5
RhysMarch <c1012902@newcastle.ac.uk>
2024-01-31 03:27:02 +0000

    Sidebar added w/ scroll to section on click. Also, remove logo and button on smaller browsers.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,34 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const menuButton = document.querySelector('.menu.btn14');
-    menuButton.addEventListener('click', function() {
-        this.classList.toggle('open');
+document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        var sidebar = document.getElementById("mySidebar");
+        var main = document.getElementById("main");
+        sidebar.style.width = '0';
+        main.classList.remove("sidebar-open");
+
+        const href = this.getAttribute('href');
+        const offsetTop = document.querySelector(href).offsetTop;
+
+        const fixedHeaderHeight = document.querySelector('.title-container').clientHeight;
+        const additionalOffset = 100; // This value makes it scroll to the top, plus a bit more
+
+        window.scroll({
+            top: offsetTop - fixedHeaderHeight - additionalOffset,
+            behavior: "smooth"
+        });
     });
 });
+
+
+function toggleSidebar() {
+    var sidebar = document.getElementById("mySidebar");
+    var main = document.getElementById("main");
+
+    if (sidebar.style.width === '200px') {
+        sidebar.style.width = '0';
+        main.classList.remove("sidebar-open");
+    } else {
+        sidebar.style.width = '200px';
+        main.classList.add("sidebar-open");
+    }
+}
